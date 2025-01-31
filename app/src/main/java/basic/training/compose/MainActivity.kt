c63cb4f3516198635a153661531b3ae3ed8167e9
farhanfath <farhanfathur73@gmail.com>
2025-01-31 19:31:09 +0700

    prepare: start and prepare require dependency project

---
+++ app/src/main/java/basic/training/compose/MainActivity.kt
@@ -0,0 +1,33 @@
+package basic.training.compose
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import basic.training.compose.ui.theme.GithubAppComposeTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            MyApp {
+
+            }
+        }
+    }
+}
+
+@Composable
+fun MyApp(content: @Composable () -> Unit) {
+    GithubAppComposeTheme {
+        content()
+    }
+}
